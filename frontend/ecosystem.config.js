5bd94e7f3c1998207fc34565ec6b92c3afa91cc7
alexeyrumyantsev <rumyantsevale02@sbsit.com>
2025-05-07 21:47:10 +0300

    8

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
+      'post-deploy': `export PATH=/home/${DEPLOY_USER}/.nvm/versions/node/v14.21.3/bin:$PATH && npm --prefix frontend ci && npm --prefix frontend run build`,
     },
   },
 };
